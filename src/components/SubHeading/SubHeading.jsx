eaddd685671bf392ec8ed86ce1103aa9ec00a974
Omer <eomereu@gmail.com>
2022-07-28 00:23:47 +0200

    Adding animations to elements and retidying responsive (media) thresholds

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { images } from '../../constants'
 
 const SubHeading = (props) => (
-  <div style={{ marginBottom: '1rem' }}>
+  <div id={props.id} style={{ marginBottom: '1rem' }}>
     <p className='p__cormorant'>{props.title}</p>
     <img src={images.spoon} alt='spoon' className='spoon_img' />
   </div>
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,12 +7,15 @@ import './Header.css';
 
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
+    {/* info div */}
     <div className='app__wrapper_info'>
-      <SubHeading title='Chase the new flavour' />
+      <SubHeading title='Chase the new flavour' id='header__subheading' />
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
       <p className='p__opensans' style={{ margin: '2rem 0'}} p={1}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus purus nec odio convallis accumsan. Vivamus semper viverra sapien id posuere. Proin cursus justo et porttitor interdum. In pharetra tristique suscipit. Nam dapibus erat vitae diam interdum volutpat. Duis quis ultricies massa. Morbi condimentum sapien dui, a ullamcorper lorem vestibulum in. Nullam quis odio ut arcu ornare blandit eget id turpis. Donec commodo ex quis mattis viverra.</p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
+
+    {/* image div */}
     <div className='app__wrapper_img'>
       <img src={images.welcome} alt="header img" />
     </div>
